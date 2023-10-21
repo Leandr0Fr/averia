5b759d0cd654a98a5019923f3cd20e1e28533030
leandroFr <leandro00fr@gmail.com>
2023-10-21 10:48:37 -0300

    ADD upload image

--- src/routes/predict.py
+++ src/routes/predict.py
@@ -1,9 +1,19 @@
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, reqparse
 from .response_generation import response_generation
+from werkzeug.datastructures import FileStorage
 
 ns_predict = Namespace("predict")
-
+# Define un analizador de solicitud para manejar la carga de archivos
+parser = reqparse.RequestParser()
+parser.add_argument('image', type=FileStorage, location='files', required=True, help='Image file')
 @ns_predict.route("")
 class Predict(Resource):
+    @ns_predict.expect(parser)
     def post(self):
-        return response_generation({"message" : "Endpoint conectado"}, 200)
+        args = parser.parse_args()
+        image = args['image']
+        if image.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image.save("image.png")
+            return response_generation({"message" : "todo piola"}, 200)
+        else:
+            return response_generation({"message" : "I'm a teapot!"}, 418)
