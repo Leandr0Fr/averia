19d9f19c48533abb53c4da1dcf210f7a57314890
leandroFr <leandro00fr@gmail.com>
2023-10-21 12:36:03 -0300

    ADD boolean symptoms

--- src/routes/predict.py
+++ src/routes/predict.py
@@ -1,4 +1,4 @@
-from flask_restx import Resource, Namespace, reqparse
+from flask_restx import Resource, Namespace, reqparse, inputs
 from .response_generation import response_generation
 from werkzeug.datastructures import FileStorage
 
@@ -6,11 +6,19 @@ ns_predict = Namespace("predict")
 # Define un analizador de solicitud para manejar la carga de archivos
 parser = reqparse.RequestParser()
 parser.add_argument('image', type=FileStorage, location='files', required=True, help='Image file')
-@ns_predict.route("")
+#ACORDARSE DEL inputs.boolean
+parser.add_argument('debilidad_focal', type=inputs.boolean, help='debilidad_focal')
+parser.add_argument('convulsiones', type=inputs.boolean, help='convulsiones')
+parser.add_argument('perdida_visual', type=inputs.boolean, help='perdida_visual')
+
+@ns_predict.route("/fred")
 class Predict(Resource):
     @ns_predict.expect(parser)
     def post(self):
         args = parser.parse_args()
+        debilidad = args['debilidad_focal']
+        convulsiones = args['convulsiones']
+        perdida_visual = args['perdida_visual']
         image = args['image']
         if image.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
             image.save("image.png")
