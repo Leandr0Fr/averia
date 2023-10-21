14102f99a13d23a713b52b3d71dde8d912943171
leandroFr <leandro00fr@gmail.com>
2023-10-21 13:58:22 -0300

    ADD prediction_pneumonia

--- src/routes/model.py
+++ src/routes/model.py
@@ -20,4 +20,17 @@ def prediction_tumor():
     return list(zip(class_labels, probabilities))
 
 def prediction_pneumonia():
-    return None
+      #Carga el modelo.
+    model = tensorflow.keras.models.load_model("routes/neumonia-resnet.h5")
+    img = Image.open("routes/image.png").convert("L")
+    #Transforma la imagen para que coincidan con el modelo. 
+    img = img.resize((224, 224)) 
+    img = np.array(img)
+    img = img / 255.0
+    #Predice el modelo 
+    predict = model.predict(img)[0]
+    #Como son 4 clases, entonces es una lista de 4 elementos.
+    class_labels = ["Pneumonia", "No_Pneumonia"]
+    #Genera una lista con los % de acierto.
+    probabilities = [(prob * 100) for prob in predict]
+    return list(zip(class_labels, probabilities))
