2c4b27807b53fff8c5a9805b14c6ab5f55e4643a
leandroFr <leandro00fr@gmail.com>
2023-10-21 14:29:23 -0300

    FIX open image

--- src/routes/model.py
+++ src/routes/model.py
@@ -22,15 +22,14 @@ def prediction_tumor():
 def prediction_pneumonia():
       #Carga el modelo.
     model = tensorflow.keras.models.load_model("routes/neumonia-resnet.h5")
-    img = Image.open("routes/image.png").convert("L")
-    #Transforma la imagen para que coincidan con el modelo. 
-    img = img.resize((224, 224)) 
-    img = np.array(img)
-    img = img / 255.0
-    #Predice el modelo 
-    predict = model.predict(img)[0]
-    #Como son 4 clases, entonces es una lista de 4 elementos.
-    class_labels = ["Pneumonia", "No_Pneumonia"]
-    #Genera una lista con los % de acierto.
+# Carga la imagen en formato RGB
+    img = Image.open("routes/image.png").convert("RGB")
+    img = img.resize((224, 224))  # Asegúrate de que la imagen tenga el tamaño adecuado
+    # Convierte la imagen en un arreglo NumPy si es necesario
+    img_array = np.array(img)
+    img_array = img_array / 255
+    # Realiza la predicción
+    class_labels = ["Pneunomia", "No_pneunomia"]
+    predict = model.predict(np.expand_dims(img_array, axis=0))[0]
     probabilities = [(prob * 100) for prob in predict]
     return list(zip(class_labels, probabilities))
--- src/routes/predict.py
+++ src/routes/predict.py
@@ -1,7 +1,7 @@
 from flask_restx import Resource, Namespace, reqparse, inputs
 from .response_generation import response_generation
 from werkzeug.datastructures import FileStorage
-from .model import prediction_tumor
+from .model import prediction_tumor, prediction_pneumonia
 ns_predict = Namespace("predict")
 # Define un analizador de solicitud para manejar la carga de archivos
 parser = reqparse.RequestParser()
@@ -10,7 +10,6 @@ parser.add_argument('image', type=FileStorage, location='files', required=True, 
 parser.add_argument('debilidad_focal', type=inputs.boolean, help='debilidad_focal')
 parser.add_argument('convulsiones', type=inputs.boolean, help='convulsiones')
 parser.add_argument('perdida_visual', type=inputs.boolean, help='perdida_visual')
-
 
 @ns_predict.route("/fred")
 class Predict(Resource):
@@ -26,6 +25,26 @@ class Predict(Resource):
             image.save("routes/image.png")
             response_data = {}
             class_probabilities = prediction_tumor()
+
+            for tumor_type, probability in class_probabilities:
+                response_data[tumor_type.lower()] = probability
+            
+            return response_generation(response_data, 200)
+        else:
+            return response_generation({"message" : "I'm a teapot!"}, 418)
+        
+parser_wini = reqparse.RequestParser()
+parser_wini.add_argument('image', type=FileStorage, location='files', required=True, help='Image file')
+@ns_predict.route("/wini")
+class Predict(Resource):
+    @ns_predict.expect(parser_wini)
+    def post(self):
+        args = parser_wini.parse_args()
+        image = args['image']
+        if image.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image.save("routes/image.png")
+            response_data = {}
+            class_probabilities = prediction_pneumonia()
 
             for tumor_type, probability in class_probabilities:
                 response_data[tumor_type.lower()] = probability
