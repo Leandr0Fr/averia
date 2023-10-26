d5c5edd0bc0fa38af92b8bd12ba4cca5c138a973
leandroFr <leandro00fr@gmail.com>
2023-10-26 16:42:39 -0300

    ADD append_predict_wini def

--- src/routes/utils.py
+++ src/routes/utils.py
@@ -4,3 +4,12 @@ def exists_id(csv, id):
     df = pd.read_csv(csv)
     list_id = df['id'].tolist()
     return id in list_id
+
+def append_predict_wini(id, imagen, puntada_lateral, fiebre, dificultad_respiratoria):
+    df = pd.read_csv("csv/wini.csv")
+    new_row = pd.DataFrame({'id' : [id], 'imagen' : [imagen], 'puntada_lateral' : [puntada_lateral],
+                             'fiebre' : [fiebre], 'dificultad_respiratoria' : [dificultad_respiratoria],
+                             'pneumonia' : [''], 'no_pneumonia' : ['']})
+    
+    df = pd.concat([df, new_row], ignore_index=True)
+    df.to_csv('csv/wini.csv', index=False)
