fc776d150d616858c072eb0dd317eafa808a6030
leandroFr <leandro00fr@gmail.com>
2023-10-26 22:31:50 -0300

    FIX directory csv

--- src/routes/utils.py
+++ src/routes/utils.py
@@ -6,37 +6,37 @@ def exists_id(csv, id):
     return id in list_id
 
 def append_predict_wini(id, imagen, puntada_lateral, fiebre, dificultad_respiratoria):
-    df = pd.read_csv("wini.csv")
+    df = pd.read_csv("csv/wini.csv")
     new_row = pd.DataFrame({'id' : [id], 'imagen' : [imagen], 'puntada_lateral' : [puntada_lateral],
                              'fiebre' : [fiebre], 'dificultad_respiratoria' : [dificultad_respiratoria],
                              'pneumonia' : [''], 'no_pneumonia' : ['']})
     
     df = pd.concat([df, new_row], ignore_index=True)
-    df.to_csv('wini.csv', index=False)
+    df.to_csv('csv/wini.csv', index=False)
 
 def append_feedback_wini(id, pneumonia, no_pneumonia):
-    df = pd.read_csv("wini.csv")
+    df = pd.read_csv("csv/wini.csv")
     
     df.loc[df['id'] == id, 'pneumonia'] = pneumonia
     df.loc[df['id'] == id, 'no_pneumonia'] = no_pneumonia
 
-    df.to_csv("wini.csv", index=False, float_format='%.0f')
+    df.to_csv("csv/wini.csv", index=False, float_format='%.0f')
 
 def append_predict_fred(id, imagen, debilidad_focal, convulsiones, perdida_visual):
-    df = pd.read_csv("fred.csv")
+    df = pd.read_csv("csv/fred.csv")
     new_row = pd.DataFrame({'id' : [id], 'imagen' : [imagen], 'debilidad_focal' : [debilidad_focal],
                              'convulsiones' : [convulsiones], 'perdida_visual' : [perdida_visual],
                              'glioma' : [''], 'meningioma' : [''], 'pituitary' : [''], 'no_tumor' : ['']})
     
     df = pd.concat([df, new_row], ignore_index=True)
-    df.to_csv('fred.csv', index=False)
+    df.to_csv('csv/fred.csv', index=False)
 
 def append_feedback_fred(id, glioma, meningioma, pituitary, no_tumor):
-    df = pd.read_csv("fred.csv")
+    df = pd.read_csv("csv/fred.csv")
     
     df.loc[df['id'] == id, 'glioma'] = glioma
     df.loc[df['id'] == id, 'meningioma'] = meningioma
     df.loc[df['id'] == id, 'pituitary'] = pituitary
     df.loc[df['id'] == id, 'no_tumor'] = no_tumor
 
-    df.to_csv("fred.csv", index=False, float_format='%.0f')
+    df.to_csv("csv/fred.csv", index=False, float_format='%.0f')
