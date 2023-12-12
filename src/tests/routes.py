bce3fa49293d82b63d639eaeb61bf1aec940211d
LeandroFr <leandro00fr@gmail.com>
2023-12-12 07:59:46 -0300

    CREATED routes images_test

---
+++ src/tests/routes.py
@@ -0,0 +1,6 @@
+#rutas relacionadas con imagenes.
+NO_TUMOR_IMAGE = 'image_test/no_tumor.jpg'
+GLIOMA_IMAGE = "image_test/glioma.jpg"
+MENINGIOMA_IMAGE = "image_test/meningioma.jpg"
+PITUTARY_IMAGE = "image_test/pituitary.jpg"
+INVALID_FILE = "image_test/invalid.txt"
